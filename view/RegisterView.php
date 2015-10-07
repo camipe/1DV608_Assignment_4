1b6cb186c6ca7e17aef39ec95889a48db0b547eb
Micael Persson <micael.c.persson@gmail.com>
2015-10-07 20:52:06 +0200

    Updated RegisterView to use Bootstrap CSS.

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -19,16 +19,21 @@ class RegisterView {
 				<fieldset>
 					<legend> Register - enter Username and password</legend>
 					<p id='".self::$messageID."'>$message</p>
-					<label for='".self::$username."'>Username :</label>
-					<input type='text' id='".self::$username."' name='".self::$username."' value=''/>
-					<br/>
-					<label for='".self::$password."'>Password :</label>
-					<input type='password' id='".self::$password."' name='".self::$password."'/>
-					<br/>
-					<label for='".self::$rptPassword."'>Repeat Password :</label>
-					<input type='password' id='".self::$rptPassword."' name='".self::$rptPassword."'/>
-					<br/>
-					<input type='submit' name='".self::$register."' value='Register'/>
+					<div class='form-group'>
+						<label for='".self::$username."'>Username :</label>
+						<input type='text' id='".self::$username."' class='form-control' name='".self::$username."' value=''/>
+					</div>
+					<div class='form-group'>
+						<label for='".self::$password."'>Password :</label>
+						<input type='password' id='".self::$password."' class='form-control' name='".self::$password."'/>
+					</div>
+					<div class='form-group'>
+						<label for='".self::$rptPassword."'>Repeat Password :</label>
+						<input type='password' id='".self::$rptPassword."' class='form-control' name='".self::$rptPassword."'/>
+					</div>
+					<div class='form-group'>
+						<input type='submit' name='".self::$register."' class='btn btn-default' value='Register'/>
+					</div>
 				</fieldset>
 			</form>";
 	}
